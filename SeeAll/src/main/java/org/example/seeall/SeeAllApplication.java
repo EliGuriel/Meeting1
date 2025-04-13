dca9699e4d16f91b8b06f167d43a689c790c5c49
Eli <eliguriel1@gmail.com>
2025-04-13 23:43:54 +0300

    Version 1.10 of Meeting 1 Advanced JAVA B

---
+++ SeeAll/src/main/java/org/example/seeall/SeeAllApplication.java
@@ -0,0 +1,13 @@
+package org.example.seeall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeeAllApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeeAllApplication.class, args);
+    }
+
+}
---
+++ SeeAll/src/test/java/org/example/seeall/SeeAllApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.seeall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeeAllApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
